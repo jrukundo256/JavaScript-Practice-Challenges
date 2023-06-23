33f16e0831e32741fee58bc3925d5dbc515b1515
Jonathan Rukundo <rukundojonathan360@gmail.com>
2023-06-23 20:18:15 +0300

    Solving set 1, question 1

--- set1/1one.js
+++ set1/1one.js
@@ -5,3 +5,13 @@
 // are expected to be strings. The function should return the string Fizz if
 // the combined length of the parameters is divisible by 3, the string Buzz
 // if it is divisible by 5, and the string FizzBuzz if it is divisible by both 5 and 3.
+
+// Solution:
+
+const fizzBuzz = (str1, str2) => {
+    const combinedLength = `${str1}${str2}`.length;
+    return (combinedLength % 3 === 0 && combinedLength % 5 === 0) ? "FizzBuzz" :
+        (combinedLength % 3 === 0) ? "Fizz" :
+            (combinedLength % 5 === 0) ? "Buzz" :
+                "";
+};
