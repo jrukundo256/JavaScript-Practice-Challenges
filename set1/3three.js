4242eb31af95cbd7be3dfc95cc6dec2dfe928c86
Jonathan Rukundo <rukundojonathan360@gmail.com>
2023-06-24 23:07:45 +0300

    Solving set 1, question 3..

--- set1/3three.js
+++ set1/3three.js
@@ -8,3 +8,17 @@
 // you may not use the same element twice.
 
 // You can return the answer in any order.
+
+
+// Solution:
+
+const twoSum = (nums, target) => {
+    for (let i = 0; i < (nums.length) - 1; i++) {
+        for (let j = (i + 1); j < (nums.length); j++) {
+            if ((nums[i] + nums[j]) === target) return [i, j];
+        }
+    }
+}
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([6, 3, 4, 2], 6));
