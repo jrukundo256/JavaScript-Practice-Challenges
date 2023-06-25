06bb822b254c4d954667cf76da4dbb24d95a7bd2
Jonathan Rukundo <rukundojonathan360@gmail.com>
2023-06-25 20:29:10 +0300

    Solving set 2, querstion 1..

--- set2/1one.js
+++ set2/1one.js
@@ -3,3 +3,21 @@
 
 // Given an integer num, write a function that repeatedly adds all its digits
 // until the result has only one digit, and return it.
+
+
+// Solution:
+
+const addDigits = num => {
+    if (num < 10) return num;
+
+    let sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
+    }
+    
+    return addDigits(sum);
+  };
+
+  
+  console.log(addDigits(38));
